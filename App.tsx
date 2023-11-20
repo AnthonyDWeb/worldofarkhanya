8eaa5241ab36c4a09a0b476df87382643c162ba3
AnthonyDWeb <delforge.anthony@gmail.com>
2023-11-21 00:03:40 +0100

    deploy aoo test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Worl Of Arkhanya!</Text>
       <StatusBar style="auto" />
     </View>
   );
